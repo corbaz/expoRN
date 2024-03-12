2811447dcc4cbdc84f51481b12b94f5e631dfdec
Julio Cesar Corbaz <julio.corbaz@gmail.com>
2024-03-12 10:21:31 -0300

    Primer commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={ styles.title}>EXPO JCC!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 60,
+    fontWeight: 'bold',
+    color: 'white',
+  }
 });
---
+++ README.md
@@ -0,0 +1,18 @@
+npx create-expo-app expoRN
+✔ Downloaded and extracted project files.
+✔ Installed JavaScript dependencies.
+
+✅ Your project is ready!
+
+To run your project, navigate to the directory and run one of the following npm commands.
+
+- cd expoRN
+- npm run android
+- npm run ios
+- npm run web
+
+A new version of `create-expo-app` is available
+You can update by running: npm install -g create-expo-app
+
+lsof -i :8081
+kill 93928
