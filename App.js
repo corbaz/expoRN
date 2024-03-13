3d1a07a2650ad62b3be239ae7e0903c2060c459d
Julio Cesar Corbaz <julio.corbaz@gmail.com>
2024-03-13 07:17:43 -0300

    Estilos diferentes en distintas plataforma

--- App.js
+++ App.js
@@ -20,9 +20,9 @@ export default function App() {
           styles.title,
           {
             color: Platform.select({
-              ios: "cyan",
+              ios: "fuchsia",
               android: "green",
-              web: "red",
+              web: "white",
             }),
           },
         ]}
